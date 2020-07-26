515c4d1dfbd2abbb8dead267ce32661ba91e4f91
stefan.genov <stefantigro@gmail.com>
2020-07-26 20:17:40 +0300

    [master] Upgraded to latest ER version

--- event_request_with_rate_limiting.js
+++ event_request_with_rate_limiting.js
@@ -1,12 +1,15 @@
-const app	= require( 'event_request' )();
+const { App, Loggur }	= require( 'event_request' );
+const app				= App();
 
-const rule	= {
+const rule				= {
 	maxAmount: 500,
 	refillTime: 1,
 	refillAmount: 500,
 	policy: 'strict',
 	ipLimit: false
 };
+
+Loggur.disableDefault();
 
 app	.get( '/', app.er_rate_limits.rateLimit( rule ), event =>{ event.send( '' ); } )
 	.listen( 80, ()=>{ console.log( 'Event Request with rate Limiting Started' ); } );
