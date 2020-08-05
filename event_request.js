75823555f7d4d8a86c1a8c6c0a17559c9dbfaeb7
stefan.genov <stefantigro@gmail.com>
2020-08-05 23:41:51 +0300

    [master] update

--- event_request.js
+++ event_request.js
@@ -1,4 +1,4 @@
 const app	= require( 'event_request' )();
 
-app	.get( '/', event =>{ event.send( '' ); } )
+app	.get( '/', event =>{ event.response.end( '' ); } )
 	.listen( 80, ()=>{ console.log( 'Event Request Started' ); } );
